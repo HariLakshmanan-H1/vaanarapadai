9b75457c4ace46c1b025c10549b50774125d78d5
HariLakshmanan-H1 <138225938+HariLakshmanan-H1@users.noreply.github.com>
2026-03-06 21:18:02 +0530

    dbug4

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,38 +5,26 @@ import { prisma } from "./prisma"
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "database" },
+
+  session: {
+    strategy: "database",
+  },
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+
   callbacks: {
     async session({ session, user }) {
-      if (session.user) session.user.id = user.id
+      if (session.user) {
+        (session.user as any).id = user.id
+      }
       return session
     },
   },
+
   secret: process.env.NEXTAUTH_SECRET,
-  cookies: {
-    sessionToken: {
-      name: `next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-    state: {
-      name: `next-auth.state`,
-      options: {
-        httpOnly: true,
-        sameSite: "lax",
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-      },
-    },
-  },
 }
